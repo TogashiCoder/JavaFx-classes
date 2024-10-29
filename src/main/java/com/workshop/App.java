701735ff5ee0bbdf41e3058fdff212da9422b0d3
Taoufik <silverproduction2023@gmail.com>
2024-10-29 19:17:47 +0100

    simple program using xml and Controller

--- src/main/java/com/workshop/App.java
+++ src/main/java/com/workshop/App.java
@@ -1,20 +1,9 @@
 package com.workshop;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.image.Image;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
